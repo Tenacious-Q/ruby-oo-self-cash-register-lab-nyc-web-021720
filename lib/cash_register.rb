3a228be201a3f63bead34f779cbf28b903dcee51
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-25 15:06:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
 
+class CashRegister
+
+  attr_accessor :total, :discount
+
+  def initialize(discount = nil)  # must use exact vqriable name 'discount'
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1) 
+    @price = price
+    @total = price
+  end
+
+end
